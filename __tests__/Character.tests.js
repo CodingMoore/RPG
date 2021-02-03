d956d86b93ae8fc1c064e8f19d73b9495385b7cb
Randel M <Randel.C.Moore@gmail.com>
2021-02-03 13:30:18 -0800

    purposefully fail store generic character test;
    Co-authored-by: Kevin Kirkley <kevinmkirkley@gmail.com>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

--- __tests__/Character.tests.js
+++ __tests__/Character.tests.js
@@ -2,13 +2,18 @@ import {storeState, genChar} from '../src/js/Character.js'
 
 describe('character', () => {
 
-  // const Bob;
-  
-  // beforeEach(() => {
-  //   Bob = storeState(genChar);
-  // });
+  let Bob;
+  beforeEach(() => {
+    Bob = storeState(genChar);
+  });
 
   test("This will test that state exists", () => {
     expect(storeState()()).toEqual({});
   });
+
+  test("Sucessfully store a generic character 'Bob'.", () => {
+    expect(Bob()).toEqual({health: 100, armor: 0, defense: 50, strength: 50, attack: 1});
+  });
+
+
 });
