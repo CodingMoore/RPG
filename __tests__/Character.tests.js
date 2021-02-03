54943c3c0ce7fdb49236084c1d2e692b7956b027
Randel M <Randel.C.Moore@gmail.com>
2021-02-03 13:48:16 -0800

    purposefully fail changeState test
    Co-authored-by: Kevin Kirkley <kevinmkirkley@gmail.com>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

--- __tests__/Character.tests.js
+++ __tests__/Character.tests.js
@@ -1,4 +1,4 @@
-import {storeState, genChar} from '../src/js/Character.js'
+import {storeState, genChar, changeState} from '../src/js/Character.js'
 
 describe('character', () => {
 
@@ -13,6 +13,13 @@ describe('character', () => {
 
   test("Sucessfully store a generic character 'Bob'.", () => {
     expect(Bob()).toEqual({health: 100, armor: 0, defense: 50, strength: 50, attack: 1});
+  });
+  test("Test the ability to change state.", () => {
+    expect(changeState("health")(1)(Bob()).health).toEqual(101);
+    expect(changeState("armor")(1)(Bob()).armor).toEqual(1);
+    expect(changeState("defense")(1)(Bob()).defense).toEqual(51);
+    expect(changeState("strength")(1)(Bob()).strength).toEqual(51);
+    expect(changeState("attack")(1)(Bob()).attack).toEqual(2);
   });
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   devtool: 'eval-source-map',
   devServer: {
+
     contentBase: './dist'
   },
   plugins: [
