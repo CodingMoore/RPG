7e9e8b27a4558a46d5e66d14407069c08a4ad7e2
Randel M <Randel.C.Moore@gmail.com>
2021-02-03 13:19:23 -0800

    purposefully fail storeState test
    Co-authored-by: Kevin Kirkley <kevinmkirkley@gmail.com>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

--- __tests__/Character.tests.js
+++ __tests__/Character.tests.js
@@ -2,13 +2,13 @@ import {storeState, genChar} from '../src/js/Character.js'
 
 describe('character', () => {
 
-  const Bob;
+  // const Bob;
   
-  beforeEach(() => {
-    Bob = storeState(genChar);
-  });
+  // beforeEach(() => {
+  //   Bob = storeState(genChar);
+  // });
 
   test("This will test that state exists", () => {
-    expect(stateControl()).toEqual(initialState);
+    expect(storeState()).toEqual({});
   });
 });
