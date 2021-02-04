1421f7a54dd6465057211e0ede1dcd8b70e21385
Randel M <Randel.C.Moore@gmail.com>
2021-02-04 11:50:19 -0800

    prep for push

--- src/js/Character.js
+++ src/js/Character.js
@@ -4,10 +4,12 @@ export const changeState = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
-      [prop] : (state[prop] || 0) + value
+      [prop] : (state[prop] || 0) + (value || -state[prop])
     })
   }
 }
+
+// let myData = results && results.data;
 
 export const storeState = (initialState = {}) => {
   let currentState = initialState;
@@ -18,7 +20,8 @@ export const storeState = (initialState = {}) => {
   }
 }
 
-export const healthToZero = changeState("health")(-100);
+export const healthToZero = changeState("health")(0);
+export const blowToHead = changeState("health")(-100);
 export const arrowToGut = changeState("health")(-50);
 export const arrowToKnee = changeState("health")(-25);
 
@@ -32,3 +35,43 @@ export const equipShield = changeState("armor")(25);
 export const Bob = storeState(genChar);
 
 
+if (Bob().health <= 0) {
+  console.log("Bob is DEAD!")
+}
+
+
+// const genChar = {health: 100, armor: 0, defense: 50, strength: 50, attack: 1}
+
+// const changeState = (prop) => {
+//   return (value) => {
+//     return (state) => ({
+//       ...state,
+//       [prop] : (state[prop] || 0) + value
+//     })
+//   }
+// }
+
+// const storeState = (initialState = {}) => {
+//   let currentState = initialState;
+//   return (stateChangeFunction = state => state) => {
+//     const newState = stateChangeFunction(currentState);
+//     currentState = {...newState};
+//     return newState;
+//   }
+// }
+
+// const healthToZero = changeState("health")(-(changeState("health")));
+// const arrowToGut = changeState("health")(-50);
+// const arrowToKnee = changeState("health")(-25);
+
+// const healthPotion = changeState("health")(50);
+// const bigHealthPotion = changeState("health")(100);
+
+// const arrowHitsShield = changeState("armor")(-10);
+// const equipShield = changeState("armor")(25);
+
+// const Bob = storeState(genChar);
+
+// if (Bob().health <= 0) {
+//   console.log("Bob is DEAD!")
+// }
